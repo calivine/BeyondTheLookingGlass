7f8800300fb324db306f2fd834d991fb32629118
Alex Caloggero <a.calog@protonmail.com>
2018-09-18 13:26:01 -0400

    Removed file(s) from path.

--- itineraryscript.js
+++
@@ -1,8 +0,0 @@
-var check_in = document.getElementById("in").innerHTML;
-var check_out = document.getElementById("out").innerHTML;
-var check_in_date = new Date(check_in);
-var check_out_date = new Date(check_out);
-var checkin = check_in_date.toDateString();
-var checkout = check_out_date.toDateString();
-document.getElementById("in").innerHTML = checkin;
-document.getElementById("out").innerHTML = checkout;
